58d3b9519781b97f3a9430d13f37312f859151a3
Menna  Dessokey <>
2018-07-18 12:26:38 +0200

    Player control (right left and jump- doesnt fall) and gun aiming

---
+++ Assets/AIM.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIM : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+        
+            //rotation
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = 5.23f;
+
+            Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+            mousePos.x = mousePos.x - objectPos.x;
+            mousePos.y = mousePos.y - objectPos.y;
+
+            float angle = Mathf.Atan2(mousePos.y, mousePos.x) * 2 *Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 2, angle));
+
+	}
+}
---
+++ Assets/playcontroller.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playcontroller : MonoBehaviour {
+
+    public float speed;
+   // public float threshold;
+    private Rigidbody2D rb;
+
+      public Transform spawnPoint;//Add empty gameobject as spawnPoint
+    public float minHeightForDeath;
+    public GameObject player; //Add your player
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void  Update () 
+    {
+          if (Input.GetKeyDown ("space")){
+                  transform.Translate(Vector2.up * 260 * Time.deltaTime, Space.World);
+          } 
+
+
+    
+        if (player.transform.position.y < minHeightForDeath)
+            player.transform.position = spawnPoint.position;
+ } 
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        //if (transform.position.y < threshold)
+            //transform.position = new Vector3(0, 0, 0);
+    }
+
+
+   
+ 
+
+         
+}
