b7946780701602c77377d48da0d499404b8b33f1
Youssef-PC\Youssef <youssefshosny@gmail.com>
2018-07-22 20:00:49 +0200

    fixed

--- Assets/Scripts/CamerController.cs
+++ Assets/Scripts/CamerController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class CamerController : MonoBehaviour {
 
     public Transform playerLocation;
-    public float xDifference;
-    public float yDifference;
+    public float x;
+    public float y;
 
 
     // Use this for initialization
@@ -18,7 +18,7 @@ public class CamerController : MonoBehaviour {
 	// Update is called once per frame
 	void LateUpdate () {
 
-        transform.position = new Vector3(playerLocation.position.x + xDifference,yDifference,-29);
+        transform.position = new Vector3(playerLocation.position.x + x,y,-29);
 
     }
 }
