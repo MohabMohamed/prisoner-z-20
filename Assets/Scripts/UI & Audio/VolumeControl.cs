cf5801855ac460be887095a9666afac8250f6c43
Youssef-PC\Youssef <youssefshosny@gmail.com>
2018-07-27 18:24:05 +0200

    volumecontrol/new gunshot sound

---
+++ Assets/Scripts/UI & Audio/VolumeControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeControl : MonoBehaviour {
+
+    private Slider volSlider;
+	// Use this for initialization
+	void Start () {
+        volSlider = this.gameObject.GetComponent<Slider>();
+        volSlider.value = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        AudioListener.volume = volSlider.value;
+    }
+}
