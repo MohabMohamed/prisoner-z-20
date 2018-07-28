bd2d5db567427a68f9b61e617963f694fff01756
Yousef <youssefshosny@gmail.com>
2018-07-28 14:34:07 +0200

    Camera Boundaries

--- Assets/Scripts/UI & Audio/CameraController.cs
+++ Assets/Scripts/UI & Audio/CameraController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,17 +7,41 @@ public class CameraController : MonoBehaviour {
 
     public Transform playerLocation;
     public float yPos;
-
+    public float zPos;
     public float speed;
 
+/*
+    public float leftBoundary;
+    public float rightBoundary;
     public Transform CameraTarget;
+    */
+    public Transform CameraTarget;
+    public Transform leftBoundary;
+    public Transform rightBoundary;
+
+    public float leftBoundaryDiff;
+    public float rightBoundaryDiff;
+    
 
 	// Update is called once per frame
 	void LateUpdate () {
 
         //transform.position = new Vector3(playerLocation.position.x + x,y,-29);
 
-        transform.position = Vector3.Slerp(transform.position, new Vector3(CameraTarget.position.x, yPos, transform.position.z), Time.deltaTime * speed);
+        
+        if( !(transform.position.x < leftBoundary.position.x + leftBoundaryDiff) | !(transform.position.x > rightBoundary.position.x + rightBoundaryDiff) )
+            transform.position = Vector3.Slerp(transform.position, new Vector3(CameraTarget.position.x, yPos, zPos), Time.deltaTime * speed);
+        
+        if(transform.position.x < leftBoundary.position.x + leftBoundaryDiff)
+        {
+            //transform.position = new Vector3(leftBoundary , transform.position.y, zPos);
+            transform.position = Vector3.Slerp(transform.position, new Vector3(leftBoundary.position.x + leftBoundaryDiff, yPos, zPos), Time.deltaTime * speed);
+        } else if(transform.position.x > rightBoundary.position.x + rightBoundaryDiff)
+        {
+            //transform.position = new Vector3(rightBoundary , transform.position.y, zPos);
+            transform.position = Vector3.Slerp(transform.position, new Vector3(rightBoundary.position.x + rightBoundaryDiff, yPos, zPos), Time.deltaTime * speed);
+        }
+
 
 
     }
