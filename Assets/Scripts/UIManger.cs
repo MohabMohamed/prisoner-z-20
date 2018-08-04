1749da4dbbeb23220275b0935729c0233125c47f
MohabMohamed <mohab.m.mohamed@gmail.com>
2018-08-04 02:49:54 +0200

    Pause Menu

--- Assets/Scripts/UIManger.cs
+++ Assets/Scripts/UIManger.cs
@@ -3,37 +3,60 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 public class UIManger : MonoBehaviour {
 
+    static public bool inGame, isPaused;
+    GameObject PuaseMenuUI;
+    Button quitButton,resumeButton,mainMenuButton;
 
     public Dropdown resolutionDropdown;
     Resolution[] resolutions;
 
     public void Start()
     {
-        resolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
-        List<string> options = new List<string>();
-        int curResIdx = 0;
-        string option;
-        for (int i = 0; i < resolutions.Length; i++)
-        {
-            option = resolutions[i].width + " X " + resolutions[i].height;
-            options.Add(option);
-            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
-                curResIdx = i;
-        }
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = curResIdx;
-        resolutionDropdown.RefreshShownValue();
+        GetAvailResolutions();
+        inGame = isPaused = false;
     }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && inGame)
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    private void Pause()
+    {
+        PuaseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    private void Resume()
+    {
+        PuaseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        isPaused = false;
+    }
+
     public void Play()
     {
+        inGame = true;
         FindObjectOfType<AudioManger>().Stop("MainMenuTheme");
         Debug.Log("Play");
         UnityEngine.SceneManagement.SceneManager.LoadScene(1);
         FindObjectOfType<AudioManger>().Play("GamePlayTheme");
+        Invoke("InGameUiAwake", 3.0f);
+
 
     }
 
@@ -54,11 +77,54 @@ public class UIManger : MonoBehaviour {
     {
         Screen.fullScreen = isFullScreen;
     }
+    void GetAvailResolutions()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int curResIdx = 0;
+        string option;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            option = resolutions[i].width + " X " + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+                curResIdx = i;
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = curResIdx;
+        resolutionDropdown.RefreshShownValue();
+    }
 
     public void SetResolution(int resolutionIdx)
     {
         Screen.SetResolution(resolutions[resolutionIdx].width,
             resolutions[resolutionIdx].height, Screen.fullScreen);
     }
+    void InGameUiAwake()
+    {
+        GetPauseMenu();
+        resumeButton.onClick.AddListener(Resume);
+        mainMenuButton.onClick.AddListener(BackToMainMenu);
+        quitButton.onClick.AddListener(Quit);
+    }
 
+    void GetPauseMenu()
+    {
+       Debug.Log(PuaseMenuUI = GameObject.Find("Canvas/PauseMenu"));
+       Debug.Log(resumeButton = GameObject.Find("Canvas/PauseMenu/ResumeButton").GetComponent<Button>());
+       Debug.Log(mainMenuButton = GameObject.Find("Canvas/PauseMenu/MainMenu").GetComponent<Button>());
+       Debug.Log(quitButton = GameObject.Find("Canvas/PauseMenu/QuitButton").GetComponent<Button>());
+    }
+
+    void BackToMainMenu()
+    {
+        Time.timeScale = 1.0f;
+        inGame = false;
+        FindObjectOfType<AudioManger>().Stop("GamePlayTheme");
+        Debug.Log("BackToMainMenu");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        FindObjectOfType<AudioManger>().Play("MainMenuTheme");
+    }
+ 
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -10,7 +10,7 @@ public class Weapon : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (!UIManger.isPaused && Input.GetKeyDown(KeyCode.Mouse0))
         {
           Bullet clone = (Bullet)Instantiate(bullet, transform.position, transform.rotation);
             clone.GetComponent<Rigidbody2D>().AddForce(clone.transform.right*bulletSpeed);
